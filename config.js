9736572c89e3654cf5e12b3bcfb9e9a79b4499d7
Arsalan <arsalantanvir3@gmail.com>
2021-07-08 20:46:29 +0500

    first comit

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const env = process.env.NODE_ENV || 'production'
 
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
-	development :{
-		APIKey : '',
-		APISecret : ''
-	},
-	production:{	
-		APIKey : '',
-		APISecret : ''
-	}
-};
+  development: {
+    APIKey: 'RQxsxS4sRh2qAyeaG_LNEQ',
+    APISecret: 'yidp6A4IJTD9je1kjxI6aumOp9FnxoH7AMs4',
+  },
+  production: {
+    APIKey: 'RQxsxS4sRh2qAyeaG_LNEQ',
+    APISecret: 'yidp6A4IJTD9je1kjxI6aumOp9FnxoH7AMs4',
+  },
+}
 
 module.exports = config[env]
